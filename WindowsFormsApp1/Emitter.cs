f9af6624b7a375b6ffe3180455b48d7d07d6a6ea
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-04-24 16:24:49 +0800

    Создана форма, создан класс частицы, создан класс поведения частиц. У частицы есть здоровье, по истечению которого частица перемещается в точку положения мыши и восстанавливает своё здоровье. Также добавлены чёрные дыри с соответсвующим поведением для частиц

---
+++ WindowsFormsApp1/Emitter.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Emitter
+    {
+        List<Particle> particles = new List<Particle>();
+            
+        public int MousePositionX;
+        public int MousePositionY;
+
+        public float GravitationX = 0;
+        public float GravitationY = 0;
+
+
+        public List<Point> gravityPoints = new List<Point>();
+        public void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                 
+                particle.Life -= 1;  // не трогаем
+                if (particle.Life < 0)
+                {
+                    // тоже не трогаем
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+
+                    
+
+                    /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+                    /* конец ЭТО ДОБАВЛЯЮ  */
+
+                    // это не трогаем
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+
+                    foreach (var point in gravityPoints)
+                    {
+                        float gX = point.X - particle.X;
+                        float gY = point.Y - particle.Y;
+                        float r2 = gX * gX + gY * gY;
+                        float M = 100;
+
+                        particle.SpeedX += (gX) * M / r2;
+                        particle.SpeedY += (gY) * M / r2;
+                    }
+
+                    // а это старый код, его не трогаем
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+
+                }
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+                {
+                    // а у тут уже наш новый класс используем
+                    var particle = new ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
+            }
+        }
+
+        public void Render(Graphics g)
+        {
+            // ну тут так и быть уж сам впишу...
+            // это то же самое что на форме в методе Render
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+                
+            foreach (var point in gravityPoints)
+            {
+                g.FillEllipse(
+                    new SolidBrush(Color.Red),
+                    point.X - 5,
+                    point.Y - 5,
+                    10,
+                    10
+                );
+            }
+        }
+    }
+}
